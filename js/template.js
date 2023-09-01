673f1cce4753f2ba5bf3386c1e68d9242570a135
neurones <neurones1968@hotmail.fr>
2023-09-01 19:39:28 +0200

    My first commit

---
+++ js/template.js
@@ -0,0 +1,11 @@
+const generateCarCard = (car) => {
+  const template = document.querySelector("#car-card").innerHTML;
+  return template.replace(/\${(.*?)}/g, (x, g) => car[g]);
+};
+
+export const appendCars = (cars) => {
+  document.getElementById("first-load").innerHTML = "";
+  if (!cars) return;
+  const cardHTML = cars.map((c) => generateCarCard(c.value)).join("");
+  document.querySelector(".mdl-grid").innerHTML += cardHTML;
+};
